0325f6ede01a205c9b1064de782d3eee98cad83b
Tim van Dam <timvandamcs@gmail.com>
2020-04-04 16:09:36 +0200

    native js errors are now returned when fetching an error with their name

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-module.exports = new Proxy({}, {
+module.exports = new Proxy({
+  Error,
+  SyntaxError,
+  RangeError,
+  ReferenceError,
+  SyntaxError,
+  TypeError,
+  URIError
+}, {
   get (target, property) {
     return target[property] || (target[property] = class extends Error {
       constructor (...args) {
